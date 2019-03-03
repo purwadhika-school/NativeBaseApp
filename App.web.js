e88e5a085625c07e0e18fe38d698d3fee58218da
yogieputra8 <yogie.putra8@gmail.com>
2019-03-03 15:25:26 +0700

    remove app.web

--- App.web.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import { Container, Header, Content, Icon, Accordion, Text, View } from "native-base";
-
-export default class App extends Component {
-  render() {
-    return <View>
-  }
-}
